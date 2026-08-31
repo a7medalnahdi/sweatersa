061a56c19213a83fcb7b99a6cb10685afcd59ff0
a7medalnahdi <a7medalnahdi.co@gmail.com>
2026-08-31 17:26:44 +0300

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+(() => {
+  'use strict';
+  window.SWEATER_FIREBASE_CONFIG = Object.freeze({
+    apiKey: 'AIzaSyBAUdbU8j_bb-EYEiOs09hxpyRCFcfEt9Q',
+    authDomain: 'sweater-work-space.firebaseapp.com',
+    projectId: 'sweater-work-space',
+    storageBucket: 'sweater-work-space.firebasestorage.app',
+    messagingSenderId: '990595632276',
+    appId: '1:990595632276:web:3eea18792cb729da007881'
+  });
+})();
